c626a3b4cd43f3db1f32ba5a5fe135b6861c0548
javierbere <javierbere@4f6ea7ea-8703-ec11-9e2c-ed81402e1204>
2010-05-14 04:48:50 +0000

    Some basic work made to Move

--- Crystalis Server/src/net/k3rnel/server/battle/Move.java
+++ Crystalis Server/src/net/k3rnel/server/battle/Move.java
@@ -2,10 +2,47 @@ package net.k3rnel.server.battle;
 
 public abstract class Move implements BattleTurn {
 	String m_name;
-	int m_baseDmg, m_baseAmmount, m_baseCoolOff;
+	protected int m_basePwr, m_baseAmmount, m_baseCoolDown, m_accuracy;
 	Type m_type;
 	
+	public enum MoveCategory{
+		NORMAL_ATTACK,
+		SPECIAL_ATTACK,
+		STAT_BOOST
+	}
+	
+	/**
+	 * Returns the type of turn performed
+	 */
 	public TurnType getTurnType() {
 		return TurnType.ATTACK;
 	}
+	
+	/**
+	 * Returns the base cooldown time for the move
+	 * @return
+	 */
+	public int getBaseCoolDown() {
+		return m_baseCoolDown;
+	}
+	
+	/**
+	 * Returns the accuracy of a move
+	 * @return
+	 */
+	public int getAccuracy() {
+		return m_accuracy;
+	}
+	/**
+	 * Returns the base power of a move
+	 * @return
+	 */
+	public int getBasePower(){
+		return m_basePwr;
+	}
+	
+	/**
+	 * Performs the action
+	 */
+	public abstract void performAction();
 }
--- Crystalis Server/src/net/k3rnel/server/battle/move/Tackle.java
+++ Crystalis Server/src/net/k3rnel/server/battle/move/Tackle.java
@@ -3,9 +3,16 @@ package net.k3rnel.server.battle.move;
 import net.k3rnel.server.battle.Move;
 
 public class Tackle extends Move{
-
+	
+	public Tackle() {
+		this.m_basePwr = 30;
+		this.m_accuracy = 100;
+		
+	}
+	
+	@Override
 	public void performAction() {
-
+		
 	}
 
 }
